e8e09e6ca1ed906742ce18102c24330b931d73f2
Keeeparis <vladimir1trotsenko@gmail.com>
2021-09-01 11:54:56 +0600

    second edition of commits - D

--- client/app/index.js
+++ client/app/index.js
@@ -1,13 +1,11 @@
-// import or require all js/css/img/fonts files "here"
-// png files are copied via copywebpackplugin - no need to include here
+        /* css */
+import './assets/css/main.scss';
+// import '../node_modules/@fortawesome/fontawesome-free/js/brands.js';
+// import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js';
 
-import './css/main.scss';
-import './js/importAllSvg.js';
-import './js/importAllJpg.js';
-import '../node_modules/@fortawesome/fontawesome-free/js/brands.js';
-import '../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js';
-import './js/cart.js';
-import './js/nav.js';
-import './js/order.js';
-import './js/carousel-images';
+        /* js */
+import './assets/js/cart.js';
+import './assets/js/nav.js';
+import './assets/js/order.js';
+import './assets/js/carousel-images';
 
--- client/app/js/importAllJpg.js
+++
@@ -1,5 +0,0 @@
-function importAll(r) {
-    r.keys().forEach(r);
-}
-  
-export default importAll(require.context('../img', true, /\.jpg$/));
